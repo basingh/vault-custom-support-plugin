f8999645c2d2e544f06160617014caeb04a22ab7
Baljeet <nagi.baljeet@gmail.com>
2023-03-17 21:51:40 +1100

    handleRead

--- backend.go
+++ backend.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"strings"
 	"sync"
 
+	//"github.com/aerospike/aerospike-client-go/v5/logger"
+	"github.com/hashicorp/errwrap"
+	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/helper/jsonutil"
 	"github.com/hashicorp/vault/sdk/logical"
 )
 
-// factory function to setup a backend in vault
-// this is mandatory function and should include call to setup backend
+// factory function to setup a backend in vaultw
 func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend, error) {
 	b := backend()
 	if err := b.Setup(ctx, conf); err != nil {
@@ -86,10 +91,10 @@ func (b *mordorBackend) paths() []*framework.Path {
 			},
 
 			Operations: map[logical.Operation]framework.OperationHandler{
-				//logical.ReadOperation: &framework.PathOperation{
-				//	Callback: b.handleRead,
-				//	Summary:  "Read secrets",
-				//},
+				logical.ReadOperation: &framework.PathOperation{
+					Callback: b.handleRead,
+					Summary:  "Read secrets",
+				},
 				// error due to https://github.com/hashicorp/vault/blob/main/sdk/framework/backend.go#L111 is looking for FieldData
 				// data coming from request, need to know how to pass these hardcoded data to this func
 				// there are some ideas here to push it directly physical storage https://github.com/hashicorp/vault/blob/main/sdk/logical/storage_inmem.go#L37-L45
@@ -112,21 +117,73 @@ func (b *mordorBackend) paths() []*framework.Path {
 	}
 }
 
+// handleRead operation to read data from path
+
+func (b *mordorBackend) handleRead(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if req.ClientToken == "" {
+		return nil, fmt.Errorf("Client token is empty")
+	}
+
+	path := data.Get("path").(string)
+	var rawData map[string]interface{}
+	entry, err := req.Storage.Get(ctx, req.ClientToken+"/"+path)
+
+	if err != nil {
+		return nil, err
+	} else if entry == nil {
+		err := logical.ErrorResponse("No value found")
+		return err, nil
+	}
+
+	fetchedData := entry.Value
+	if fetchedData == nil {
+		resp := logical.ErrorResponse("No value found at %v%v", req.MountPoint, path)
+		return resp, nil
+	}
+
+	if err := jsonutil.DecodeJSON(fetchedData, &rawData); err != nil {
+		return nil, errwrap.Wrapf("json decoding failed: {{err}}", err)
+	}
+
+	resp := &logical.Response{
+		Data: rawData,
+	}
+
+	return resp, nil
+}
+
 // handleWrite operation to write on the path
 
 func (b *mordorBackend) handleWrite(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	// store kv pair in required path
 
+	if len(req.Data) == 0 {
+		return nil, fmt.Errorf("Data cannot be nil")
+	}
 	path := data.Get("path").(string)
 
+	buf, err := json.Marshal(req.Data)
+	if err != nil {
+		return nil, errwrap.Wrapf("json encoding failed: {{err}}", err)
+	}
+
 	entry := &logical.StorageEntry{
-		Key:      "myKey" + path,
-		Value:    []byte("123"),
+		Key:      req.ClientToken + "/" + path,
+		Value:    buf,
 		SealWrap: false,
 	}
+
 	if err := req.Storage.Put(ctx, entry); err != nil {
 		return nil, err
 	}
+
+	if response, err := req.Storage.Get(ctx, entry.Key); err != nil {
+		return nil, err
+	} else {
+		logger := hclog.New(&hclog.LoggerOptions{})
+		logger.Info(string(response.Value))
+	}
+
 	resp := &logical.Response{
 		Data: map[string]interface{}{
 			entry.Key: entry,
